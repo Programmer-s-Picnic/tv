7c928a1a348ba0b4f9ef238d6258353330179a12
Champak's World <champaksworld@gmail.com>
2026-03-01 15:14:08 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,30 @@
-const CACHE = "pp-tv-v1";
-const FILES = ["./","./index.html","./manifest.webmanifest"];
+"use strict";
 
-self.addEventListener("install", e=>{
- e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
- self.skipWaiting();
+var CACHE="pp-tv-v1";
+var FILES=[
+ "./",
+ "./index.html",
+ "./manifest.webmanifest",
+ "./icons/icon-192.png",
+ "./icons/icon-512.png"
+];
+
+self.addEventListener("install",function(e){
+ e.waitUntil(
+  caches.open(CACHE).then(function(c){
+   return c.addAll(FILES);
+  })
+ );
 });
 
-self.addEventListener("activate", e=>{
+self.addEventListener("activate",function(e){
  e.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", e=>{
+self.addEventListener("fetch",function(e){
  e.respondWith(
-  caches.match(e.request).then(r=>r||fetch(e.request))
+  caches.match(e.request).then(function(r){
+   return r||fetch(e.request);
+  })
  );
 });
